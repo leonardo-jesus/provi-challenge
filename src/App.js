32bbd365c9b2b4f74104afd2f284a41cddf82220
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-06 17:11:30 -0300

    Update visual

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Grid, Card, CardHeader, CardContent, Avatar, List, ListItem, ListItemText } from '@material-ui/core';
+import PageHeader from './components/PageHeader/index.tsx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = () => ({
+  root: {
+     padding: "50px 100px",
+     zIndex: 999,
+     position: "absolute"
+   },
+   card: {
+     display: "flex",
+     height: "calc(100vh - 100px)"
+   },
+   background: {
+     position: "absolute",
+     height: 200,
+     width: "100%",
+     top: 0,
+     background: "#99d7f2"
+   },
+ });
 
-export default App;
+ const App = ({ classes }) => (
+  <div>
+    <div className={classes.background} />
+    <Grid container className={classes.root}>
+      <Grid item xs={12}>
+        <Card className={classes.card}>
+
+        </Card>
+      </Grid>
+    </Grid>
+  </div>
+);
+
+const list = [
+  { id: 1, name: "Diego", text: "Lorem ipsum", image: "" },
+  { id: 2, name: "Robson", text: "Lorem ipsum", image: "" },
+  { id: 3, name: "Cleiton", text: "Lorem ipsum", image: "" }
+];
+
+export default withStyles(styles)(App);
