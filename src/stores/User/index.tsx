9b1afe0b788ec4035f443155133f554040a1759b
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-07 15:28:58 -0300

    Add typing, React.FC and Props

---
+++ src/stores/User/index.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, FC, useState } from 'react';
+import { getUserData } from '../../api';
+
+export const UserContext = createContext<any>({});
+
+type MyProps = {
+  children: React.ReactNode
+}
+
+export const UserProvider: React.FC<MyProps> = ({ children }) => {
+  const [username, setUsername] = useState('Leonardo');
+
+  const changeNameByAPI = async () => {
+    setUsername('Loading...');
+    const name = await getUserData();
+    setUsername(name);
+  };
+
+  return <UserContext.Provider value={{ username, changeNameByAPI }}>{children}</UserContext.Provider>;
+};
+
