1491ddce6075d49d8dbe7c8ae2d40c0f62b18ce7
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-07 23:29:59 -0300

    Add PageUser with user info

--- src/components/PageUser/index.tsx
+++ src/components/PageUser/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Avatar} from '@material-ui/core';
+import { Box} from '@material-ui/core';
 import { useStyles } from './styles';
+import { PageUserAvatar } from '../PageUserAvatar';
 
 export const PageUser = () => {
   const classes = useStyles();
 
   return (
-
+    <Box className={classes.box}>
+      <PageUserAvatar />
+    </Box>
   );
 };
