5346e76bb8ad9c65373e80a36fc70c594c380eb9
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-07 22:04:51 -0300

    Add PageUser component

---
+++ src/components/PageUser/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Avatar} from '@material-ui/core';
+import { useStyles } from './styles';
+
+export const PageUser = () => {
+  const classes = useStyles();
+
+  return (
+
+  );
+};
