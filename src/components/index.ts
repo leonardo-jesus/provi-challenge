4a4fe45947c5c20d87b15f4595e7816ed72d2460
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-08 13:48:55 -0300

    Add PageUserIcons export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { PageHeader } from './PageHeader';
 export { PageUser } from './PageUser';
 export { PageUserAvatar } from './PageUserAvatar';
+export { PageUserIcons } from './PageUserIcons';
