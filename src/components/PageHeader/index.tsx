c79254e6716951b2cbb1fd7e157cdb693232bd0d
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-07 23:28:55 -0300

    Add 2 DIVs to separate the text and icons

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Toolbar, Button, Divider, Typography, Icon, ListItemIcon } from '@material-ui/core';
+import { Toolbar, Button, Divider, Typography, ListItemIcon } from '@material-ui/core';
 import { ImgCustom, AppBarCustom, useStyles } from './styles';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -8,8 +8,9 @@ export const PageHeader = () => {
   const classes = useStyles();
 
   return (
-      <AppBarCustom color="secondary" position="relative" className={classes.appBar}>
-        <Toolbar>
+    <AppBarCustom color="secondary" position="relative" className={classes.appBar}>
+      <Toolbar>
+        <div className={classes.iconDivLeft}>
           <ImgCustom></ImgCustom>
           <Divider orientation="vertical" className={classes.divider} flexItem />
           <Typography variant="h6" color="primary" className={classes.typo}>Summary</Typography>
@@ -21,12 +22,13 @@ export const PageHeader = () => {
           <Typography variant="h6" className={classes.typo}>Wallet</Typography>
           <Divider orientation="vertical" className={classes.divider} flexItem />
           <Typography variant="h6" className={classes.typo}>Shopping</Typography>
-          <div className={classes.iconDiv}>
-            <ListItemIcon className={classes.notificationsIcon}>{<NotificationsIcon />}</ListItemIcon>
-            <ListItemIcon className={classes.notificationsIcon}>{<SettingsIcon />}</ListItemIcon>
-            <Button color="primary" variant="contained" className={classes.logout}>Log Out</Button>
-          </div>
-        </Toolbar>
-      </AppBarCustom>
+        </div>
+        <div className={classes.iconDivRight}>
+          <ListItemIcon>{<NotificationsIcon />}</ListItemIcon>
+          <ListItemIcon>{<SettingsIcon />}</ListItemIcon>
+          <Button color="primary" variant="contained" className={classes.logout}>Log Out</Button>
+        </div>
+      </Toolbar>
+    </AppBarCustom>
   );
 };
