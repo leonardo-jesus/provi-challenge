32bd8bb117fa78accc54823c5d86647caab6e705
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-07 23:30:13 -0300

    Add style to PageUser

--- src/components/PageUser/styles.tsx
+++ src/components/PageUser/styles.tsx
@@ -1,44 +1,11 @@
-import styled from 'styled-components';
-import { AppBar, makeStyles } from '@material-ui/core';
-import myLogo from '../../assets/logo.png';
-
-export const ImgCustom = styled.img.attrs({
-  src: myLogo,
-})`
-  width: 150px;
-  height: auto;
-`;
-
-export const AppBarCustom = styled(AppBar)`
-  position: relative;
-  background: red;
-  height: 65px;
-  border: 0;
-  box-shadow: none;
-`;
+import { makeStyles } from '@material-ui/core';
 
 export const useStyles = makeStyles((theme) => ({
-  appBar: {
-    boxShadow: "none",
+  box: {
+    width: "99%",
+    height: "170px",
+    background: "#1496ef",
+    marginTop: "0",
+    borderTop: "2px solid #80bfda",
   },
-  divider: {
-    marginTop: "10px",
-    marginRight: 20,
-    height: "45px",
-  },
-  typo: {
-    marginRight: 20,
-    fontSize: "16px",
-    pointer: "hover",
-  },
-  logout: {
-    width: "100px",
-    marginTop: -15,
-  },
-  notificationsIcon: {
-    marginTop: 7,
-  },
-  iconDiv: {
-    marginLeft: 500,
-  }
 }));
