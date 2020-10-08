88c63e906c09c7ff47ffe16c05cc7b57d4f43588
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-08 13:49:52 -0300

    Add style to organize the components

--- src/components/PageUser/styles.tsx
+++ src/components/PageUser/styles.tsx
@@ -3,9 +3,10 @@ import { makeStyles } from '@material-ui/core';
 export const useStyles = makeStyles((theme) => ({
   box: {
     width: "99%",
-    height: "170px",
+    height: "185px",
     background: "#1496ef",
     marginTop: "0",
     borderTop: "2px solid #80bfda",
+    display: "flex",
   },
 }));
