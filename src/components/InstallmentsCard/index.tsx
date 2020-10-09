da51778200d645931eeef75b0da7420457f1007d
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-09 18:07:41 -0300

    Add props and formattation for receive userData.installments

--- src/components/InstallmentsCard/index.tsx
+++ src/components/InstallmentsCard/index.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { Card, CardActions, CardContent, Button, Typography, List, ListSubheader, ListItem, ListItemText, Divider } from '@material-ui/core';
+import React, { FC } from 'react';
+import { Card, CardActions, CardContent, Button, List, ListItem, ListItemText, Divider } from '@material-ui/core';
 import { useStyles } from './styles';
 
-export const InstallmentsCard = () => {
+type Props = {
+  installments: Array<any>;
+}
+
+export const InstallmentsCard:FC<Props> = ({ installments }) => {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
       <CardContent>
         <List className={classes.list} subheader={<li />}>
-          {[0, 1, 2, 3, 4].map((sectionId) => (
+          {installments.map((sectionId) => (
             <li key={`section-${sectionId}`} className={classes.listSection}>
               <ul className={classes.ul}>
-                {[0, 1, 2].map((item) => (
+                {installments.map((item) => (
                   <ListItem className={classes.listItem} key={`item-${sectionId}-${item}`}>
                     {/* <ListItemText primary={`Item ${item}`} /> */}
-                    <ListItemText primary={`Value: `} />
-                    <ListItemText primary={`Paid: `} />
-                    <ListItemText primary={`Due date: `} />
+                    <ListItemText primary={`Value: ${item.formatedValue}`} />
+                    <ListItemText primary={`Paid: ${item.payd ? 'Yes' : 'No'}`} />
+                    <ListItemText primary={`Due date: ${item.dueDate}`} />
                     <CardActions>
                       <Button size="medium" className={classes.buttonPos}>Pay this month</Button>
                     </CardActions>
