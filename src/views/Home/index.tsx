c47e805645069c53ad397d9097241d34153a93ca
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-09 11:07:45 -0300

    Add box to PageUserAvatar and PageUserIcons

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,16 +1,17 @@
 import React, { useContext, useEffect } from 'react';
-import { PageHeader, PageUser, PageMain } from '../../components'
-import { Background, CustomizedGrid, CustomizedCard, LoadingWrapper } from './styles';
-import { ThemeProvider, createMuiTheme, CircularProgress } from '@material-ui/core';
+import { PageHeader, BalanceCard, TaxesCard, PageUserAvatar, PageUserIcons } from '../../components'
+import { Background, CustomizedGrid, CustomizedCard, LoadingWrapper, useStyles } from './styles';
+import { ThemeProvider, createMuiTheme, CircularProgress, Box } from '@material-ui/core';
 import { UserContext } from '../../stores';
 
 export const Home = () => {
   const { loading, userData, getUserInfo } = useContext(UserContext);
+  const classes = useStyles();
   useEffect(() => {
     getUserInfo();
   }, []);
 
-  const theme =  createMuiTheme({
+  const theme = createMuiTheme({
     palette: {
       primary: {
         main: '#1496ef',
@@ -34,8 +35,14 @@ export const Home = () => {
           <CustomizedGrid>
             <CustomizedCard>
               <PageHeader />
-              <PageUser />
-              <PageMain />
+              <Box className={classes.boxUser}>
+                <PageUserAvatar />
+                <PageUserIcons />
+              </Box>
+              <Box className={classes.box}>
+                <BalanceCard title="Provi Balance" balance={userData.amountTaken} status="Available" actionTitle1="Add Money" actionTitle2="Withdraw Money" />
+                <TaxesCard amountPaid={userData.amountPayd} monthlyInterest={userData.monthlyInterest} totalAmmount={userData.totalAmountInTaxes} />
+              </Box>
             </CustomizedCard>
           </CustomizedGrid>
         </CustomizedGrid>
