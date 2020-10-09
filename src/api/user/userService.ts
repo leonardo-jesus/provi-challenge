0b39c9bc51db62bd1aa673227ec5fc7236eef898
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-09 02:14:51 -0300

    Remove fetch and add Axios

---
+++ src/api/user/userService.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const ROOT_URL = 'http://www.mocky.io';
+
+const api = axios.create({
+  baseURL: ROOT_URL,
+});
+
+export const getUserData = async () => {
+  const { data } = await api.get('/v2/5c923b0932000029056bce39');
+  return data;
+}
+
+// export const getUserData = async () => {
+//   const req = await fetch('http://www.mocky.io/v2/5c923b0932000029056bce39');
+//   const res = await req.json();
+
+//   return res.name;
+// };
