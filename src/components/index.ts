d823c22230f9934a63022e471b4b4c41a6d61101
Leonardo Jesus <leonardojesus02@outlook.com>
2020-10-09 11:07:19 -0300

    Add PageUserAvatar and PageUserIcons exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 export { PageHeader } from './PageHeader';
-export { PageUser } from './PageUser';
-export { PageMain } from './PageMain';
+export { PageUserAvatar } from './PageUserAvatar';
+export { PageUserIcons } from './PageUserIcons';
+export { BalanceCard } from './BalanceCard';
+export { TaxesCard } from './TaxesCard';
+export { InstallmentsCard } from './InstallmentsCard';
